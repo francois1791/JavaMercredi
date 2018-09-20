21dbb8f5d74898e0cda0bfac7d2488a1e4024b6e
kellyo64 <francois.flous@gmail.com>
2018-09-20 05:04:08 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+var txt;
+var r = confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+if (r == true) { 
+	txt = window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+} 
+else {
+   
+}
+
